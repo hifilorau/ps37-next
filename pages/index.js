2de0800d6718fc30aa0236fd7bb86834da7e22c8
hifilorau <tomrau@gmail.com>
2021-12-24 07:53:02 -0700

    checkin

--- pages/index.js
+++ pages/index.js
@@ -40,7 +40,7 @@ const Home = () => {
         {/* </Link> */}
           {/* { this.state.randomNumber == 3 ?  <div className="tagline">A Paradise in Space</div>  : null }  */}
           {/* { this.state.randomNumber !== 3 ? <div className="tagline">makerspace, office, and arthaüs</div> : null } */}
-          <div className="tagline">event venue arthaüs, and makerspace</div> 
+          <div className="tagline">event venue arthaüs, and creative space</div> 
         </div>
       </div>
     </div>
