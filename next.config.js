22a41968cbca1215762cc53c06e9018b3d4f71b2
hifilorau <tomrau@gmail.com>
2022-06-19 11:02:41 -0400

    chekcin

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['dl.airtable.com', 'res-3.cloudinary.com'],
+    domains: ['dl.airtable.com', 'res-3.cloudinary.com', 'res-4.cloudinary.com','res-1.cloudinary.com', 'res-2.cloudinary.com' ],
   }
 }
