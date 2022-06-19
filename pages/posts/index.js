4af95008b9444f47a82a1540b0cfe672e3c2abb7
hifilorau <tomrau@gmail.com>
2022-06-19 11:21:10 -0400

    checkin

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -4,26 +4,31 @@ import styles from '../../styles/Blog.module.css'
 import Image from 'next/image'
 import { style } from "@mui/system";
 
-const Posts = (props) => (
+const Posts = (props) => {
+  return (
   <div className={styles.blogWrapper}>
     <h1>Updates</h1>
     <ul className={styles.posts} >
-      {props.posts.map((post) => (
+      {props.posts.map((post) => {
+        const d = new Date(post.published_at)
+        const date = d.toDateString()
+        console.log('D', d) 
+        return (
         <li className={styles.postLi} key={post.id}>
           <Link href={`/posts/${post.slug}`}>
           <a className={styles.indexTitle}>
            <h3>{post.title}</h3> 
+           <div className={styles.date}>{date}</div>
            <div className={styles.imageWrapper}>
             <Image src={post.feature_image}  layout="fill" objectFit="cover"/>
            </div>
            </a>
           </Link>
         </li>
-      ))}
+      )})}
     </ul>
   </div>
-);
-
+)}
 export default Posts;
 
 export async function getServerSideProps(context) {
