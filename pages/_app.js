66178e174c6c94b6ec953e6ccf2df434248cfac0
hifilorau <tomrau@gmail.com>
2022-06-26 20:29:56 -0400

    checkin

--- pages/_app.js
+++ pages/_app.js
@@ -3,48 +3,21 @@ import '../styles/future.css'
 // import '../styles/art.css'
 import { MoralisProvider } from 'react-moralis';
 import { useRouter } from "next/router";
-import { useState, useEffect } from "react";
 // import Loading from '../components/Loading'
 // import * as ga from '../utils/ga'
 import Header from '../components/Header'
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
-  // const [loading, setLoading] = useState(false);
 
-  // const handleRouteChange = (url) => {
-  //   ga.pageview(url)
-  //   setLoading(false)
   // }
   const path = router.pathname
-  console.log('path', path)
-  // const handleRouteChange = (url) => {
-  //   console.log('URL', router)
-  //   setLoading(false)
-  //   ga.pageview(router.pathname)
-  // }
-  
-  // useEffect(() => {
 
-  //   router.events.on("routeChangeError", (e) => setLoading(false));
-  //   router.events.on("routeChangeStart", (e) => setLoading(true));
-  //   router.events.on("routeChangeComplete", handleRouteChange);
-
-  //   return () => {
-  //     console.log('ROUTER', router)
-  //     router.events.off("routeChangeError", (e) => setLoading(false));
-  //     router.events.off("routeChangeStart", (e) => setLoading(true));
-  //     router.events.off("routeChangeComplete", handleRouteChange);
-  //   };
-  // }, [router.events]);
 
   return (
     <MoralisProvider appId={process.env.NEXT_PUBLIC_APP_ID} serverUrl={process.env.NEXT_PUBLIC_SERVER_URL}>
-    {/* {loading ? <Loading /> :<> */}
      {path != "/vaporplanes" && path != "/" && <Header/> }
       <Component {...pageProps} />
-    {/* </> }= */}
-      
     </MoralisProvider>
   )
 }
